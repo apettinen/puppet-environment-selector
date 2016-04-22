2b0d9128935f8bc3835dfc7e2d53a1fecd841d91
Antti Pettinen <antti.pettinen@tut.fi>
2016-04-22 15:51:41 +0300

    hipsulointia /AP

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,14 +12,14 @@
 
 class tut_environment_selector ( String $desired_env = 'testing', String $conf_section = 'agent') {
   if $::operatingsystem == 'windows' {
-    $puppet_cmd = "puppet.bat config set environment"
+    $puppet_cmd = 'puppet.bat config set environment'
     $puppet_cmd_path = "C:\Program Files\puppet labs\puppet\bin"
   }
   else {
-    $puppet_cmd = "puppet config set environment"
+    $puppet_cmd = 'puppet config set environment'
     $puppet_cmd_path = "/opt/puppetlabs/puppet/bin/"
   }
-  
+
   exec { 'configure_environment':
     command => " $puppet_cmd $desired_env --section $conf_section",
     path    => $puppet_cmd_path,
