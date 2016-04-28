9a6f3982dd0d88518649c863b5d95a9b0d284bd4
Antti Pettinen <antti.pettinen@tut.fi>
2016-04-28 14:08:04 +0300

    typos and commas... /AP

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,13 +21,13 @@ class tut_environment_selector ( String $desired_env = 'testing', String $conf_s
   #   $puppet_cmd_path = '/opt/puppetlabs/puppet/bin/'
   # }
   $puppet_cmd = $::operatingsystem ? {
-    'windows' => 'puppet.bat config set environment'
-    default   => 'puppet config set environment'
+    'windows' => 'puppet.bat config set environment',
+    default   => 'puppet config set environment',
   }
 
   $puppet_cmd_path = $::operatingsystem ? {
-    'windows' => 'C:/Program Files/puppet labs/puppet/bin'
-    default   => '/opt/puppetlabs/puppet/bin/'
+    'windows' => 'C:/Program Files/puppet labs/puppet/bin',
+    default   => '/opt/puppetlabs/puppet/bin/',
   }
 
   notify { "Using Puppet command: ${puppet_cmd} from path: ${puppet_cmd_path}": }
