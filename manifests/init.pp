47ed2d6ab860dd3d2512b1c7c5bf8b6f2507ac06
Antti Pettinen <antti.pettinen@tut.fi>
2016-04-28 15:02:59 +0300

    set withpath => false in notify /AP

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-# Class: tut-environment-selector
+# Class: tut_environment_selector
 # Module initialization
 # This module sets the environment for client
 # via puppet config set
@@ -7,19 +7,11 @@
 # - environment: desired environment, defaults to testing
 # - section: puppet config section, defaults to agent
 #
-# Antti Pettinen
-# Copyright: Tampere University of Technology
-
+# Antti Pettinen & Sami Lanu
+# Copyright: Tampere University of Technology, IT-Services
 
 class tut_environment_selector ( String $desired_env = 'testing', String $conf_section = 'agent') {
-  # if $::operatingsystem == 'windows' {
-  #   $puppet_cmd = 'puppet.bat config set environment'
-  #   $puppet_cmd_path = 'C:/Program Files/puppet labs/puppet/bin'
-  # }
-  # else {
-  #   $puppet_cmd = 'puppet config set environment'
-  #   $puppet_cmd_path = '/opt/puppetlabs/puppet/bin/'
-  # }
+
   $puppet_cmd = $::operatingsystem ? {
     'windows' => 'puppet.bat config set environment',
     default   => 'puppet config set environment',
@@ -30,7 +22,9 @@ class tut_environment_selector ( String $desired_env = 'testing', String $conf_s
     default   => '/opt/puppetlabs/puppet/bin/',
   }
 
-  notify { "Using Puppet command: ${puppet_cmd} from path: ${puppet_cmd_path}": }
+  notify { "Using Puppet command: ${puppet_cmd} from path: ${puppet_cmd_path}":
+    withpath => false,
+  }
   exec { 'configure_environment':
     command => "${puppet_cmd} ${desired_env} --section ${conf_section}",
     path    => $puppet_cmd_path,
