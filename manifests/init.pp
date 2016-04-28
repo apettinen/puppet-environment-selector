db50072c7819212e53f497940d91713f52f689ea
Antti Pettinen <antti.pettinen@tut.fi>
2016-04-28 14:01:46 +0300

    Commit by: AP - testing with selectors instead of if-clauses

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,14 +12,24 @@
 
 
 class tut_environment_selector ( String $desired_env = 'testing', String $conf_section = 'agent') {
-  if $::operatingsystem == 'windows' {
-    $puppet_cmd = 'puppet.bat config set environment'
-    $puppet_cmd_path = 'C:\Program Files\puppet labs\puppet\bin'
+  # if $::operatingsystem == 'windows' {
+  #   $puppet_cmd = 'puppet.bat config set environment'
+  #   $puppet_cmd_path = 'C:/Program Files/puppet labs/puppet/bin'
+  # }
+  # else {
+  #   $puppet_cmd = 'puppet config set environment'
+  #   $puppet_cmd_path = '/opt/puppetlabs/puppet/bin/'
+  # }
+  $puppet_cmd = $::operatingsystem ? {
+    'windows' => 'puppet.bat config set environment'
+    default => 'puppet config set environment'
   }
-  else {
-    $puppet_cmd = 'puppet config set environment'
-    $puppet_cmd_path = '/opt/puppetlabs/puppet/bin/'
+
+  $puppet_cmd_path = $::operatingsystem ? {
+    'windows' => 'C:/Program Files/puppet labs/puppet/bin'
+    default => '/opt/puppetlabs/puppet/bin/'
   }
+
   notify { "Using Puppet command: ${puppet_cmd} from path: ${puppet_cmd_path}": }
   exec { 'configure_environment':
     command => "${puppet_cmd} ${desired_env} --section ${conf_section}",
